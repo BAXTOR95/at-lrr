2e2f69c952778e52b13b240aa8532ffba915f725
Brian Arriaga <brian.arriaga@citi.com>
2020-03-10 15:21:30 -0400

    Updating store

--- frontend/app/src/app/app.module.ts
+++ frontend/app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreRouterConnectingModule, DefaultRouterStateSerializer } from '@ngrx/router-store';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -30,7 +30,7 @@ import { environment } from '../environments/environment';
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([ AuthEffects ]),
     StoreDevtoolsModule.instrument({ logOnly: environment.production }),
-    StoreRouterConnectingModule.forRoot(),
+    StoreRouterConnectingModule.forRoot({ serializer: DefaultRouterStateSerializer }),
     CoreModule,
   ],
   entryComponents: [ AppComponent ],
